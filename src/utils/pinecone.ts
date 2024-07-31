e96812e10015bafa41c04005688a0e7096ff3958
Leifer Mendez <leifer33@gmail.com>
2024-07-31 09:50:26 +0200

    update pinecone

--- src/utils/pinecone.ts
+++ src/utils/pinecone.ts
@@ -1,15 +1,12 @@
-import { PineconeClient } from '@pinecone-database/pinecone';
+import { Pinecone } from '@pinecone-database/pinecone';
 
-if (!process.env.PINECONE_ENVIRONMENT || !process.env.PINECONE_API_KEY) {
+if (!process.env.PINECONE_API_KEY) {
   throw new Error('Pinecone environment or api key vars missing');
 }
 
 async function initPinecone() {
   try {
-    const pinecone = new PineconeClient();
-
-    await pinecone.init({
-      environment: process.env.PINECONE_ENVIRONMENT ?? '', //this is in the dashboard
+    const pinecone = new Pinecone({
       apiKey: process.env.PINECONE_API_KEY ?? '',
     });
 
